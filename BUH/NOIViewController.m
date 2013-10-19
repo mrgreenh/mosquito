1d59bc749c246f200d02d1f7b4456d86114e4d3b
John Forester <forester.john@gmail.com>
2013-10-18 20:54:23 -0400

    Initial Commit

---
+++ BUH/NOIViewController.m
@@ -0,0 +1,29 @@
+//
+//  NOIViewController.m
+//  BUH
+//
+//  Created by John Forester on 10/18/13.
+//  Copyright (c) 2013 John Forester. All rights reserved.
+//
+
+#import "NOIViewController.h"
+
+@interface NOIViewController ()
+
+@end
+
+@implementation NOIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
